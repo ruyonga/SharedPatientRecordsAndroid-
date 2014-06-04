c304a579fc121d9cd74f8650ccfdabb82f468d95
ruyonga <druyonga@gmail.com>
2014-06-04 18:51:54 +0300

    SharedPatientRecordsAndroid

---
+++ gen/com/danny/fypr/sharedpatientsrecords/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.danny.fypr.sharedpatientsrecords;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
